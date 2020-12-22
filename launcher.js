811a227c1a504727c7b4511e25aaa954e1e140c4
b-charu <74617926+b-charu@users.noreply.github.com>
2020-12-22 16:06:30 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,41 @@
+class launcher
+{
+	constructor(body,anchor)
+	{
+		
+		var options={
+		    bodyA:body,			 
+			pointB:anchor, 
+			stiffness:0.004, 
+			length:1
+			
+		}
+		
+		this.bodyA = body;
+		this.pointB = anchor;
+		this.launcher = Constraint.create(options);
+		World.add(myWorld,this.launcher);
+	}
+
+	attach(body)
+	{
+		this.launcher.bodyA = body;
+	}
+
+	fly()
+	{
+		this.launcher.bodyA = null;
+	}
+
+	display()
+	{
+		if(this.launcher.bodyA)
+		{
+			var ptA = this.bodyA.position;
+			var ptB = this.pointB
+
+			strokeWeight(3);		
+			line(ptA.x,ptA.y,ptB.x,ptB.y);
+		}
+	}
+}
